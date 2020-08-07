2fb2fb55e10ec04ff83541332ffc1bf69820f5ac
jasuriy <jasur1998gopirjonov@gmail.com>
2020-08-07 18:58:44 +0500

    Updates

---
+++ precache-manifest.d736ec7829fa08f9bf67b7fa7315c6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f94e8452c40cb18247cc0bd90d21a4",
+    "url": "/movie-app-netfix-pro/index.html"
+  },
+  {
+    "revision": "436d9f0dc0355eb0adb2",
+    "url": "/movie-app-netfix-pro/static/css/main.ce98cd78.chunk.css"
+  },
+  {
+    "revision": "6abf385672f3b8e61f1c",
+    "url": "/movie-app-netfix-pro/static/js/2.4ae68704.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app-netfix-pro/static/js/2.4ae68704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "436d9f0dc0355eb0adb2",
+    "url": "/movie-app-netfix-pro/static/js/main.db9965ed.chunk.js"
+  },
+  {
+    "revision": "548f47af0e24bf32a75b",
+    "url": "/movie-app-netfix-pro/static/js/runtime-main.f143e77f.js"
+  }
+]);
